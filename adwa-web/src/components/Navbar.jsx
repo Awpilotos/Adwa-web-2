ac72ff3c99d0f7257fc1a97d5d663e10f15a3cdc
AnaRuizS <aruiz6@uwo.ca>
2026-02-25 08:08:38 -0500

    Creado menu links

--- adwa-web/src/components/Navbar.jsx
+++ adwa-web/src/components/Navbar.jsx
@@ -1,16 +1,20 @@
 export default function Navbar(){
     return <nav className="fixed top-0 z-50 transition-all w-250 duration-300 bg-white backdrop-blur-sm">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex items-centered h-14 sm:h-16 md:20">
-                <div>
-                    <img src="/adwa-logo.svg" alt="Adwa logo" className="w-8 h-6 sm:h-8"></img>
+            <div className="flex justify-between items-center h-14 sm:h-16 md:20">
+                <div className="flex items-center space-x-2 group cursor-pointer ">
+                    <div>
+                        <img src="/adwa-logo.svg" alt="Adwa logo" className="w-8 h-6 sm:h-8"></img>
+                    </div>
+
+                    {/* Nav Links */}
+                    <div className="flex items-center space-x-6 lg:space-x-8">
+                        <a href="#">Inicio</a>
+                        <a href="#">Nosotros</a>
+                        <a href="#">Portafolio</a>
+                        <a href="#">Contacto</a>
+                    </div>
                 </div>
-                <span>
-                    <span>Inicio</span>
-                    <span>Nosotros</span>
-                    <span>Portafolio</span>
-                    <span>Contacto</span>
-                </span>
             </div>
         </div>
     </nav>;
