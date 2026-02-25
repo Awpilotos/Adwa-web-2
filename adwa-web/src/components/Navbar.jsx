0bce8c3fef40a46d91e07f089190dea61f3a805c
AnaRuizS <aruiz6@uwo.ca>
2026-02-25 07:54:33 -0500

    Creado Navbar

--- adwa-web/src/components/Navbar.jsx
+++ adwa-web/src/components/Navbar.jsx
@@ -1,3 +1,11 @@
 export default function Navbar(){
-    return <nav></nav>;
+    return <nav className="fixed top-0 z-50 transition-all w-250 duration-300 bg-white backdrop-blur-sm">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex items-centered h-14 sm:h-16 md:20">
+                <div>
+                    <img src="/adwa-logo.svg" className="CodeFlow"></img>
+                </div>
+            </div>
+        </div>
+    </nav>;
 }
