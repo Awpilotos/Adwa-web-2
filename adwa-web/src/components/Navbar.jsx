5d814a59280632b03e5fdaaea038beeabff7523b
AnaRuizS <aruiz6@uwo.ca>
2026-02-25 07:57:29 -0500

    Editado navbar

--- adwa-web/src/components/Navbar.jsx
+++ adwa-web/src/components/Navbar.jsx
@@ -3,8 +3,14 @@ export default function Navbar(){
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex items-centered h-14 sm:h-16 md:20">
                 <div>
-                    <img src="/adwa-logo.svg" className="CodeFlow"></img>
+                    <img src="/adwa-logo.svg" alt="Adwa logo" className="w-8 h-6 sm:h-8"></img>
                 </div>
+                <span>
+                    <span>Inicio</span>
+                    <span>Nosotros</span>
+                    <span>Portafolio</span>
+                    <span>Contacto</span>
+                </span>
             </div>
         </div>
     </nav>;
