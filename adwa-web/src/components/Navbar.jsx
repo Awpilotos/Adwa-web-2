bede199168276e357cc645d0de0402df847af5fd
AnaRuizS <aruiz6@uwo.ca>
2026-02-25 08:18:40 -0500

    Creado menu movil

--- adwa-web/src/components/Navbar.jsx
+++ adwa-web/src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import { Menu } from 'lucide-react';
+
 export default function Navbar(){
     return <nav className="fixed top-0 z-50 transition-all w-250 duration-300 bg-white backdrop-blur-sm">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -8,12 +10,16 @@ export default function Navbar(){
                     </div>
 
                     {/* Nav Links */}
-                    <div className="flex items-center space-x-6 lg:space-x-8">
+                    <div className="md:flex items-center space-x-6 lg:space-x-8">
                         <a href="#">Inicio</a>
                         <a href="#">Nosotros</a>
                         <a href="#">Portafolio</a>
                         <a href="#">Contacto</a>
                     </div>
+
+                    <button className="md:hidden"> 
+                        <Menu className='w-5 h-5'/>
+                    </button>
                 </div>
             </div>
         </div>
