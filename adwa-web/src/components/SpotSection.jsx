baae12a1938e6a0853e55c12c8622b3cb6437af9
Awpilotos <ana.ruiz@sgconsulting.site>
2026-03-02 18:27:53 -0500

    Creado swiper con estilos en seccion spot publicitario

--- adwa-web/src/components/SpotSection.jsx
+++ adwa-web/src/components/SpotSection.jsx
@@ -1,9 +1,20 @@
-//import ThreeDCarousel, { ThreeDCarouselItem } from "./lightswind/3d-carousel";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
+
+import slide_video_1 from '../assets/videos/incoop.mp4';
+import slide_video_2 from '../assets/videos/building_freedom.mp4';
+import slide_video_3 from '../assets/videos/livin.mp4';
 
 export default function SpotSection() {
   return (
-    <div className="min-h-screen flex justify-center ">
-      <div className="w-full max-w-6xl px-8 mt-[151px]">
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <div className="w-full max-w-6xl px-8 mb-[151px] mt-[61px]">
         
         <h1 className="text-white text-[80px] leading-[80px] tracking-[0.26px]">
           Spot
@@ -22,9 +33,52 @@ export default function SpotSection() {
         >
           Publicitarios
         </h2>
+      </div>
 
-
-
+      <div className='w-full max-w-[1200px]'>
+        <Swiper
+          effect={'coverflow'}
+          grabCursor={false}
+          centeredSlides={true}
+          loop={true}
+          initialSlide={1}
+          slidesPerView={3}
+          slideToClickedSlide={true}
+          pagination={{ clickable: true }}
+          coverflowEffect={{
+            rotate: 0,
+            stretch: 0,
+            depth: 125,
+            modifier: 2.5,
+            slideShadows:false,
+          }}
+          modules={[EffectCoverflow, Pagination]}
+          breakpoints={{
+            0: { slidesPerView: 1 },  
+            768: { slidesPerView: 3 },
+          }}
+          className="swiper_container"
+        >
+          {[slide_video_1, slide_video_2, slide_video_3].map((videoSrc, idx) => (
+            <SwiperSlide
+              key={idx}
+              className='slide'
+            >
+              <video
+                className="slide-video"
+                width="100%"
+                height="100%"
+                controls={false}
+                playsInline
+                autoPlay
+                muted
+                loop
+                src={videoSrc}
+              />
+            </SwiperSlide>
+          ))}
+          <div className="swiper-pagination"></div>
+        </Swiper>
       </div>
     </div>
   );
