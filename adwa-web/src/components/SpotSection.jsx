a3c930825fd879fcff267032399a91553734612f
AnaRuizS <aruiz6@uwo.ca>
2026-03-03 08:38:04 -0500

    Arreglado contenedor videos componente spot publicitarios

--- adwa-web/src/components/SpotSection.jsx
+++ adwa-web/src/components/SpotSection.jsx
@@ -1,4 +1,5 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { useState } from 'react';
 
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
@@ -7,17 +8,29 @@ import 'swiper/css/navigation';
 
 import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
 
-import slide_video_1 from '../assets/videos/incoop.mp4';
-import slide_video_2 from '../assets/videos/building_freedom.mp4';
-import slide_video_3 from '../assets/videos/livin.mp4';
+import video_1 from "../assets/videos/incoop.mp4";
+import video_2 from '../assets/videos/building_freedom.mp4';
+import video_3 from '../assets/videos/livin.mp4';
 
 export default function SpotSection() {
+
+  const [activeVideo, setActiveVideo] = useState(null);
+
+  const videos = [
+    { thumb: "", src: video_1 },
+    { thumb: "", src: video_2 },
+    { thumb: "", src: video_3 },
+    { thumb: "", src: video_1 },
+    { thumb: "", src: video_2 },
+    { thumb: "", src: video_3 },
+  ];
+  
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
-      <div className="w-full max-w-6xl px-8 mb-[151px] mt-[61px]">
+    <div className="min-h-screen flex justify-center">
+      <div className="w-full max-w-[1400px] px-8 mt-[61px] mb-[151px]">
         
-        <h1 className="text-white text-[80px] leading-[80px] tracking-[0.26px]">
-          Spot
+        <h1 className="text-white text-[80px] leading-[80px] tracking-[0.26px] text-left">
+          Spots
         </h1>
 
         <h2
@@ -26,59 +39,50 @@ export default function SpotSection() {
             text-[90px]
             leading-[90px]
             tracking-[0.26px]
-            bg-[linear-gradient(90deg,rgba(151,0,255,1)_37%,rgba(0,183,159,1)_100%)]
+            bg-gradient-to-r
+            from-[#9700FF]
+            to-[#00B79F]
             bg-clip-text
-            text-transparent
-          "
-        >
+            text-transparent"
+          >
           Publicitarios
         </h2>
-      </div>
 
-      <div className='w-full max-w-[1200px]'>
-        <Swiper
-          effect={'coverflow'}
-          grabCursor={false}
-          centeredSlides={true}
-          loop={true}
-          initialSlide={1}
-          slidesPerView={3}
-          slideToClickedSlide={true}
-          pagination={{ clickable: true }}
-          coverflowEffect={{
-            rotate: 0,
-            stretch: 0,
-            depth: 125,
-            modifier: 2.5,
-            slideShadows:false,
-          }}
-          modules={[EffectCoverflow, Pagination]}
-          breakpoints={{
-            0: { slidesPerView: 1 },  
-            768: { slidesPerView: 3 },
-          }}
-          className="swiper_container"
-        >
-          {[slide_video_1, slide_video_2, slide_video_3].map((videoSrc, idx) => (
-            <SwiperSlide
-              key={idx}
-              className='slide'
-            >
-              <video
-                className="slide-video"
-                width="100%"
-                height="100%"
-                controls={false}
-                playsInline
-                autoPlay
-                muted
-                loop
-                src={videoSrc}
-              />
-            </SwiperSlide>
-          ))}
-          <div className="swiper-pagination"></div>
-        </Swiper>
+        <div /*className="w-full flex justify-center"*/ className='container'>
+
+          <Swiper
+            className='swiper-container'
+            effect="coverflow"
+            centeredSlides={true}
+            slidesPerView={2}
+            initialSlide={1}
+            spaceBetween={0}
+            loop={true}
+            coverflowEffect={{
+              rotate: 0,
+              stretch: 0,
+              depth: 150,
+              modifier: 1.5,
+              slideShadows: false,
+            }}
+            modules={[EffectCoverflow]}
+            style={{ width: "100%", maxWidth: "1400px" }}
+          >
+            {videos.map((video, i) => (
+              <SwiperSlide key={i}>
+                <video
+                  className="!w-[800px] !h-[500px] object-cover"
+                  src={video.src}
+                  autoPlay
+                  muted
+                  loop
+                  playsInline
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          
+        </div>
       </div>
     </div>
   );
