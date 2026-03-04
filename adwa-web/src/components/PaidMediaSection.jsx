430f95745528af6631ca0b4df6b4d3a2fe4a8021
Awpilotos <ana.ruiz@sgconsulting.site>
2026-03-04 16:44:52 -0500

    Creada sección Productora. Video con clippath creado.

--- adwa-web/src/components/PaidMediaSection.jsx
+++ adwa-web/src/components/PaidMediaSection.jsx
@@ -1,5 +1,188 @@
+import { ImageList } from '@mui/material';
+import { ImageListItem } from '@mui/material';
+import { Typography } from '@mui/material';
+import { motion } from 'framer-motion';
+
 export default function PaidMediaSection(){
-  return <div></div>
+  const imagenes=[
+        {
+            id: 1,
+            value: "+200",
+            description: "Campañas\ngestionadas",
+            src: "../../img/gradiente_azul_rosa_morado.png",
+            alt: "Gradiente",
+            dimensions: { width: "214px", height: "273px" },
+            aspectRatio: "0.79",
+            pd:0,
+        },
+        {
+            id: 2,
+            value: "",
+            description: "",
+            src: "../../img/fondo_transparente_d.png",
+            alt: "Gradiente rosa morado",
+            dimensions: { width: "214px", height: "273px" },
+            aspectRatio: "0.79",
+            pd:-118,
+        },
+        {
+            id: 3,
+            value: "",
+            description: "",
+            src: "../../img/gradiente_rosa_peq.png",
+            alt: "Gradiente",
+            dimensions: { width: "214px", height: "273px" },
+            aspectRatio: "0.79",
+            pd:265,
+        },
+        {
+            id: 4,
+            value: "",
+            description: "",
+            src: "../../img/claqueta.png",
+            alt: "Claqueta",
+            dimensions: { width: "214px", height: "273px" },
+            aspectRatio: "0.79",
+            pd:0,
+        },
+        {
+            id: 5,
+            value: "98%",
+            description: "Tasa de Éxito",
+            src: "../../img/gradiente_rosa_peq.png",
+            alt: "Gradiente",
+            dimensions: { width: "207px", height: "511px" },
+            aspectRatio: "2.47",
+            pd:0,
+        },
+        {
+            id: 6,
+            value: "",
+            description: "",
+            src: "../../img/fondo_transparente_.png",
+            alt: "Gradiente rosa morado",
+            dimensions: { width: "214px", height: "273px" },
+            aspectRatio: "0.79",
+            pd:0,
+        },
+        {
+            id: 7,
+            value: "+10",
+            description: "En medios\ndigitales",
+            src: "../../img/gradiente_rosa_morado.png",
+            alt: "Gradiente rosa morado",
+            dimensions: { width: "214px", height: "273px" },
+            aspectRatio: "0.79",
+            pd:0,
+        },
+        {
+            id: 8,
+            value: "",
+            description: "",
+            src: "../../img/gradient_mas_a.svg",
+            alt: "Claqueta",
+            dimensions: { width: "214px", height: "273px" },
+            aspectRatio: "0.79",
+            pd:0,
+        },
+        {
+            id: 9,
+            value: "+50",
+            description: "Marcas\nNacionales e Internacionales",
+            src: "../../img/gradiente_azul_rosa.png",
+            alt: "Claqueta filmación",
+            dimensions: { width: "214px", height: "273px"},
+            aspectRatio: "0.79",
+            pd:0,
+        },
+        {
+            id: 10,
+            value: "",
+            description: "",
+            src: "../../img/pieza_ajedrez.png",
+            alt: "Pieza ajedrez emporium",
+            dimensions: {height: "511px" },
+            aspectRatio: "2.47",
+            pd:0,
+        },
+        {
+            id: 11,
+            value: "+50",
+            description: "Clientes Satisfechos",
+            src: "../../img/gradiente_azul_rosa.png",
+            alt: "Gradiente",
+            dimensions: {height: "511px"},
+            aspectRatio: "2.47",
+            pd:0,
+        }
+    ];
+    const partners = [
+      { name: "Atomik_", opacity: "text-[#ffffff80]" },
+      { name: "Kinesso_", opacity: "text-[#e4e4e4]" },
+      { name: "IPG_", opacity: "text-white" },
+      { name: "Opteam_", opacity: "text-[#e4e4e4]" },
+      { name: "Estudio Tess_", opacity: "text-[#ffffff80]" },
+    ];
+
+    return <div className="min-h-screen flex flex-col justify-center items-center">
+      <div className="w-full max-w-[1000px] px-8 mt-[61px] mb-[151px]">
+        <ImageList variant='masonry' cols={4} gap={8}>
+            <ImageListItem >
+                <div className="flex flex-col items-start relative">
+                    <img
+                    src="../../img/fondo_transparente_.png"
+                    loading="lazy"
+                    style={{
+                        display: 'block',
+                        height: '400px',
+                    }}></img>
+                    <h2 className="mt-[-1.00px] font-normal text-white text-[90px] tracking-[0.26px] leading-[90px]">
+                        Paid
+                    </h2>
+
+                    <div className="bg-[linear-gradient(90deg,rgba(151,0,255,1)_37%,rgba(0,183,159,1)_100%)]
+                        [-webkit-background-clip:text] bg-clip-text
+                        [-webkit-text-fill-color:transparent]
+                        text-[100px] tracking-[0.26px] leading-[100px] font-normal">
+                        Media
+                    </div>
+                </div>
+            </ImageListItem>
+
+                
+            {imagenes.map((item, index) => (
+            <ImageListItem key={index} sx={{ pt: `${item.pd}px` }}>
+                <img
+                src={item.src}
+                alt={item.alt}
+                loading="lazy"
+                style={{
+                    display: 'block',
+                    width: '100%',
+                }}
+                />
+            </ImageListItem>
+            ))}
+          </ImageList>
+      </div>
+
+      <div className='w-full items-start '>
+        <h2 className="text-left font-semibold text-white text-[40px] leading-10 tracking-[-0.31px]">
+          Partners
+        </h2>
+        
+        <p className="w-full text-center whitespace-nowrap overflow-hidden text-8xl leading-[96px] tracking-[-0.31px]">
+          {partners.map((partner, index) => (
+            <span
+              key={index}
+              className={`${partner.opacity} inline-block tracking-[-0.30px]`}
+            >
+              {partner.name}
+            </span>
+          ))}
+        </p>
+      </div>
+    </div>;
     /*const statistics = [
     {
       id: 1,
@@ -231,4 +414,5 @@ export default function PaidMediaSection(){
       </div>
     </section>
   );*/
+
 }
--- adwa-web/src/components/ProductoraSection.jsx
+++ adwa-web/src/components/ProductoraSection.jsx
@@ -1,166 +1,55 @@
-import Masonry from '@mui/lab/Masonry';
-import { ImageList } from '@mui/material';
-import { ImageListItem } from '@mui/material';
-
+import video_4 from "../assets/videos/tras_camara.mp4";
 
 export default function ProductoraSection(){
+    return <div className="min-h-screen flex justify-center items-center">
+      <div className="relative w-full h-screen flex max-w-[1400px] px-8 mt-[61px] mb-[151px] items-center justify-center">
+        <svg
+            className="absolute inset-0 z-0 w-full"
+            viewBox="0 0 1438 1729"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <defs>
+            <clipPath id="videoClip">
+                {/* your white rectangles */}
+                <rect width="297" height="1101" x="222" y="355" />
+                <rect width="297" height="1597" x="571" y="47" />
+                <rect width="297" height="1086" x="920" y="301" />
+            </clipPath>
+            </defs>
 
-    const imagenes=[
-        {
-            id: 1,
-            value: "+200",
-            description: "Campañas\ngestionadas",
-            src: "../../img/gradiente_azul_rosa_morado.png",
-            alt: "Gradiente",
-            dimensions: { width: "214px", height: "273px" },
-            aspectRatio: "0.79",
-            pd:0,
-        },
-        {
-            id: 2,
-            value: "",
-            description: "",
-            src: "../../img/fondo_transparente_d.png",
-            alt: "Gradiente rosa morado",
-            dimensions: { width: "214px", height: "273px" },
-            aspectRatio: "0.79",
-            pd:-118,
-        },
-        {
-            id: 3,
-            value: "",
-            description: "",
-            src: "../../img/gradiente_rosa_peq.png",
-            alt: "Gradiente",
-            dimensions: { width: "214px", height: "273px" },
-            aspectRatio: "0.79",
-            pd:265,
-        },
-        {
-            id: 4,
-            value: "",
-            description: "",
-            src: "../../img/claqueta.png",
-            alt: "Claqueta",
-            dimensions: { width: "214px", height: "273px" },
-            aspectRatio: "0.79",
-            pd:0,
-        },
-        {
-            id: 5,
-            value: "98%",
-            description: "Tasa de Éxito",
-            src: "../../img/gradiente_rosa_peq.png",
-            alt: "Gradiente",
-            dimensions: { width: "207px", height: "511px" },
-            aspectRatio: "2.47",
-            pd:0,
-        },
-        {
-            id: 6,
-            value: "",
-            description: "",
-            src: "../../img/fondo_transparente_.png",
-            alt: "Gradiente rosa morado",
-            dimensions: { width: "214px", height: "273px" },
-            aspectRatio: "0.79",
-            pd:0,
-        },
-        {
-            id: 7,
-            value: "+10",
-            description: "En medios\ndigitales",
-            src: "../../img/gradiente_rosa_morado.png",
-            alt: "Gradiente rosa morado",
-            dimensions: { width: "214px", height: "273px" },
-            aspectRatio: "0.79",
-            pd:0,
-        },
-        {
-            id: 8,
-            value: "",
-            description: "",
-            src: "../../img/gradient_mas_a.svg",
-            alt: "Claqueta",
-            dimensions: { width: "214px", height: "273px" },
-            aspectRatio: "0.79",
-            pd:0,
-        },
-        {
-            id: 9,
-            value: "+50",
-            description: "Marcas\nNacionales e Internacionales",
-            src: "../../img/gradiente_azul_rosa.png",
-            alt: "Claqueta filmación",
-            dimensions: { width: "214px", height: "273px"},
-            aspectRatio: "0.79",
-            pd:0,
-        },
-        {
-            id: 10,
-            value: "",
-            description: "",
-            src: "../../img/pieza_ajedrez.png",
-            alt: "Pieza ajedrez emporium",
-            dimensions: {height: "511px" },
-            aspectRatio: "2.47",
-            pd:0,
-        },
-        {
-            id: 11,
-            value: "+50",
-            description: "Clientes Satisfechos",
-            src: "../../img/gradiente_azul_rosa.png",
-            alt: "Gradiente",
-            dimensions: {height: "511px"},
-            aspectRatio: "2.47",
-            pd:0,
-        }
-    ];
+            {/* Video cortado con la forma del svg */}
+            <foreignObject
+                x="0"
+                y="0"
+                width="1438"
+                height="1729"
+                clipPath="url(#videoClip)"
+            >
+            <video
+                src= {video_4}
+                autoPlay
+                muted
+                loop
+                playsInline
+                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+            />
+            </foreignObject>
+        </svg>
 
+        <div className="relative z-10 flex flex-col items-center text-center">
+            <h2 className="mt-[-1px] font-normal text-white text-[85px] tracking-[0.26px] leading-[90px]">
+            Productora aliada
+            </h2>
 
-    return <div className="min-h-screen flex justify-center">
-      <div className="w-full max-w-[1000px] px-8 mt-[61px] mb-[151px]">
-        <ImageList variant='masonry' cols={4} gap={8}>
-            <ImageListItem >
-                <div className="flex flex-col items-start relative">
-                    <img
-                    src="../../img/fondo_transparente_.png"
-                    loading="lazy"
-                    style={{
-                        display: 'block',
-                        height: '400px',
-                    }}></img>
-                    <h2 className="mt-[-1.00px] font-normal text-white text-[90px] tracking-[0.26px] leading-[90px]">
-                        Paid
-                    </h2>
-
-                    <div className="bg-[linear-gradient(90deg,rgba(151,0,255,1)_37%,rgba(0,183,159,1)_100%)]
-                        [-webkit-background-clip:text] bg-clip-text
-                        [-webkit-text-fill-color:transparent]
-                        text-[100px] tracking-[0.26px] leading-[100px] font-normal">
-                        Media
-                    </div>
-                </div>
-            </ImageListItem>
-
-                
-            {imagenes.map((item, index) => (
-            <ImageListItem key={index} sx={{ pt: `${item.pd}px` }}
->
-                <img
-                src={item.src}
-                alt={item.alt}
-                loading="lazy"
-                style={{
-                    display: 'block',
-                    width: '100%',
-                }}
-                />
-            </ImageListItem>
-            ))}
-        </ImageList>
-      </div>
-        
-    </div>;
+            <div
+            className="bg-[linear-gradient(90deg,rgba(151,0,255,1)_37%,rgba(0,183,159,1)_100%)]
+            [-webkit-background-clip:text] bg-clip-text
+            [-webkit-text-fill-color:transparent]
+            text-[95px] tracking-[0.26px] leading-[100px] font-normal"
+            >
+            Creativa
+            </div>
+        </div>
+    </div>
+</div>;
 }
