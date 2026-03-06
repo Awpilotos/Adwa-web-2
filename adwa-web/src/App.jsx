230c33f814ef39932b98a010300d28c1aecc262d
AnaRuizS <aruiz6@uwo.ca>
2026-03-06 07:08:56 -0500

    Incluido imagenes en la seccion campanas

--- adwa-web/src/App.jsx
+++ adwa-web/src/App.jsx
@@ -1,23 +1,10 @@
-import Navbar from "./components/Navbar";
-import PaidMediaSection from "./components/PaidMediaSection";
-import Hero from "./components/Hero";
-import SpotSection from "./components/SpotSection";
-import ProductoraSection from "./components/ProductoraSection";
-import IASection from "./components/IASection";
-import CampanasSection from './components/CampanasSection';
-import Footer from "./components/Footer";
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from './Routes';
 
 export default function App() {
   return (
-      <>
-          <Navbar/>
-          <Hero/>
-          <SpotSection/>
-          <PaidMediaSection/>
-          <ProductoraSection/>
-          <CampanasSection/>
-          <IASection/>
-          <Footer/>
-      </>
+    <Router>
+      <AppRoutes />
+    </Router>
   );
 }
--- adwa-web/src/components/CampanasSection.jsx
+++ adwa-web/src/components/CampanasSection.jsx
@@ -1,27 +1,52 @@
+
+import video_4 from "../assets/videos/building_freedom.mp4";
+
 export default function CampanaSection(){
     return <div className="section-wrapper justify-center">
-      <div className="section-container-large">
-        <div>
+      <div className="section-container-large flex flex-col lg:flex-row items-stretch gap-8 lg:gap-16">
+        <div className="flex flex-col text-left justify-between">
+            
             <h2 className="text-white text-[50px] leading-[50px] lg:text-[80px] lg:leading-[80px] tracking-[0.26px] text-left">
                 Campañas
+                <span className="relative inline-block">
+                    <span
+                        className="
+                        inline-block
+                        text-[60px] leading-[55px] lg:text-[90px] lg:leading-[90px]
+                        tracking-[0.26px]
+                        bg-gradient-to-r
+                        from-[#9700FF]
+                        to-[#00B79F]
+                        bg-clip-text
+                        text-transparent"
+                    >
+                    Publicitarias
+                    </span>
+                    <img
+                        src="../../img/gradiente_horizontal_rosa_azul.png"
+                        alt=""
+                        className="absolute left-0 bottom-[-60px] w-full h-auto pointer-events-none"
+                    />
+                </span>
             </h2>
-            <h3
-            className="
-                inline-block
-                text-[60px] leading-[55px] lg:text-[90px] lg:leading-[90px]
-                tracking-[0.26px]
-                bg-gradient-to-r
-                from-[#9700FF]
-                to-[#00B79F]
-                bg-clip-text
-                text-transparent"
+            
+            
+            <video
+                autoPlay
+                src={video_4}
+                muted
+                loop
+                playsInline
+                className="w-full h-auto rounded-xl"
             >
-                Publicitarias
-            </h3>
+            </video>
         </div>
-        <div>
-            <img src="../../img/fondo_transparente_.png">
-            </img>
+        <div className="flex justify-center items-center w-full lg:w-auto">
+            <img 
+                src="../../img/mobile-mockup.png" 
+                alt="Campaña" 
+                className="max-w-full h-auto object-contain"
+            />
         </div>
 
 
