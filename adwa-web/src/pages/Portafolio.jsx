e035af454fcd5b6e6e546ef0afd2d7674384db5b
Awpilotos <ana.ruiz@sgconsulting.site>
2026-03-09 19:29:46 -0500

    Añadidas imágenes de portafolio y grid

--- adwa-web/src/pages/Portafolio.jsx
+++ adwa-web/src/pages/Portafolio.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
+import video_livin from "../assets/videos/livin.mp4";
+
 export default function Portafolio() {
   return (
     <>
@@ -37,12 +39,12 @@ export default function Portafolio() {
            </h1>
         </div>
       </div>
+      {/* Contenido principal */}
       <div className="section-wrapper bg-[linear-gradient(296deg,rgba(39,22,51,1)_18%,rgba(28,57,142,0.2)_100%),linear-gradient(0deg,rgba(0,0,0,1)_0%,rgba(0,0,0,1)_100%)]">
-        <div className="max-w-[1400px] mx-auto px-6 flex flex-col gap-32 ">
-
-          {/* LIVIN SECTION */}
-          <div className="grid lg:grid-cols-2 gap-16 items-center">
-
+        <div className="section-container_large mx-auto px-6 flex flex-col gap-8">
+          {/*  Primera fila - Livin */}
+          <div className="flex gap-6 lg:grid-cols-2 items-center">
+            {/*  Livin - logo imagen */}
             <div className="flex flex-col gap-6">
               <img
                 className="w-full max-w-[560px] aspect-[1.84] object-cover"
@@ -56,9 +58,10 @@ export default function Portafolio() {
               </p>
             </div>
 
+            {/* Telefono mockup */}
             <div className="flex justify-center items-center">
               {/* Imagen Livin izquierda */}
-              <div className="hidden md:block relative z-10 -mr-24">
+              <div className="relative z-10 -mr-24">
                 <img
                   className="w-[320px] shadow-xl"
                   src="https://c.animaapp.com/0cn5eMNB/img/img-5561-1@2x.png"
@@ -67,7 +70,7 @@ export default function Portafolio() {
               </div>
 
               {/* Mockup */}
-              <div className="relative z-20 transform scale-[1.35] mb-[80px]">
+              <div className="relative z-20 transform scale-[1.35]">
                 <img
                   className="w-[320px] md:w-[360px]"
                   src="../../img/iphone_livin_mockup.png"
@@ -76,7 +79,7 @@ export default function Portafolio() {
               </div>
 
               {/* Imagen Livin derecha */}
-              <div className="hidden md:block relative z-10 -ml-24">
+              <div className="relative z-10 -ml-24">
                 <img
                   className="w-[320px] shadow-xl"
                   src="https://c.animaapp.com/0cn5eMNB/img/img-5561-1-1@2x.png"
@@ -84,16 +87,32 @@ export default function Portafolio() {
                 />
               </div>
             </div>
-
           </div>
 
-          {/* SECOND SECTION */}
-          <div className="grid lg:grid-cols-2 gap-16 items-center">
+          {/*  Segunda fila - Niu y Opteam */}
+          <div className="flex gap-6 items-center">
+            {/*  Primera columna Niu */}
             <div className="flex flex-col gap-6">
               <img
                 className="w-full max-w-[560px] aspect-[1.84] object-cover"
-                src="../../img/livin_fondo.png"
+                src="../../img/niu_fondo.png"
                 alt=""
+              />
+              <p className="text-white text-xl max-w-[520px]">
+                <span className="font-black block">Niu</span>
+                messaging and full design of the campaign which appeared across
+                New York this autumn.
+              </p>
+            </div>
+            {/*  Segunda columna Opteam */}
+            <div className="flex flex-col gap-6">
+              <video
+                className="w-full aspect-[1.84] object-cover"
+                src={video_livin}
+                alt="Video Livin"
+                autoPlay
+                muted
+                loop
               />
               <p className="text-white text-xl max-w-[520px]">
                 <span className="font-black block">Opteam</span>
@@ -101,48 +120,13 @@ export default function Portafolio() {
                 New York this autumn.
               </p>
             </div>
-            
-            <div className="flex flex-col gap-6">
-              <img
-                className="w-full max-w-[560px] aspect-[1.84] object-cover"
-                src="../../img/livin_fondo.png"
-                alt=""
-              />
-              <p className="text-white text-xl max-w-[520px]">
-                <span className="font-black block">Opteam</span>
-                messaging and full design of the campaign which appeared across
-                New York this autumn.
-              </p>
-            </div>
-            
           </div>
+          <div>
 
           </div>
-
-          {/* SECOND IMAGE ROW */}
-          <div className="grid grid-cols-2 md:grid-cols-3 gap-8">
-
-            <img
-              className="w-full aspect-[0.8]"
-              src="https://c.animaapp.com/0cn5eMNB/img/img-5562-1@2x.png"
-              alt=""
-            />
-
-            <img
-              className="w-full aspect-[0.8]"
-              src="https://c.animaapp.com/0cn5eMNB/img/img-5563-1@2x.png"
-              alt=""
-            />
-
-            <img
-              className="w-full aspect-[0.8]"
-              src="https://c.animaapp.com/0cn5eMNB/img/img-5564-1@2x.png"
-              alt=""
-            />
-
-          </div>
-
         </div>
+
+      </div>
       <Footer/>
     </>);
 }
