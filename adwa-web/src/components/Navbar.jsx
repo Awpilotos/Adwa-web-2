2ef7ceb65a0eb38a8ef981cc5fec47e0e7697283
Awpilotos <ana.ruiz@sgconsulting.site>
2026-03-13 16:19:26 -0500

    Creado enrutamiento de páginas internas

--- adwa-web/src/components/Navbar.jsx
+++ adwa-web/src/components/Navbar.jsx
@@ -1,25 +1,25 @@
-import { Menu } from 'lucide-react';
 import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import RoutingMenuItem from "./ui/RoutingMenuItem";
 
 export default function Navbar(){
     const [activeIndex, setActiveIndex] = useState(0);
+    const navigate = useNavigate();
 
     const navigationItems = [
         { label: "Inicio",
             path:"/",
         },
         { label: "Nosotros",
-            path:"/",
+            path:"",
         },
         { label: "Portafolio",
-            path:"/",
+            path:"/portafolio",
         },
         { label: "Contacto",
-            path:"/",
+            path:"",
         },
     ];
-
-    const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false);
 
     return (<div className='w-full sticky top-[33px] h-[90px] flex z-50 items-start justify-center'>
         <div className="flex-1 flex justify-center">
@@ -36,25 +36,8 @@ export default function Navbar(){
         
         {/* Botones de navegacion */}
         <nav className="z-50 flex px-4 py-2 w-full max-w-[425px] mx-auto items-center justify-center bg-[#ffffff1a] rounded-[28px] backdrop-blur-[2.0px] backdrop-brightness-[100.0%] backdrop-saturate-[96.5%] [-webkit-backdrop-filter:blur(2.0px)_brightness(100.0%)_saturate(96.5%)] shadow-[inset_1px_0_0_rgba(255,255,255,0.32),inset_-1px_0_1px_rgba(0,0,0,0.11)]">
-            {navigationItems.map((item, index) => (
-            <button
-                key={index}
-                onClick={() => setActiveIndex(index)}
-                className={`
-                    inline-flex items-center justify-center gap-2.5
-                    px-2 py-1 md:px-4 md:py-2
-                    rounded-2xl font-nunito font-normal text-white text-base
-                    transition-all duration-300
-                    ${activeIndex === index
-                    ? `backdrop-blur-[12px] backdrop-brightness-[108%] backdrop-saturate-[105%] backdrop-hue-rotate-[-3.6deg]
-                        [-webkit-backdrop-filter:blur(12px)_brightness(108%)_saturate(105%)_hue-rotate(-3.6deg)]
-                        shadow-[inset_0_1px_0_rgba(255,255,255,0.40),inset_1px_0_0_rgba(255,255,255,0.32),inset_0_-1px_8px_rgba(0,0,0,0.20),inset_-1px_0_8px_rgba(0,0,0,0.16)]
-                        bg-[linear-gradient(90deg,rgba(151,0,255,0.2)_37%,rgba(0,183,159,0.2)_100%)]`
-                    : ""}
-                `}
-                >
-                {item.label}
-            </button>
+            {navigationItems.map((item, index) => ( 
+                <RoutingMenuItem path={item.path} label={item.label}/>
             ))}
         </nav>
 
---
+++ adwa-web/src/components/ui/RoutingMenuItem.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+
+function RoutingMenuItem({path="/", label=""}) {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const isActive = location.pathname === path;
+
+  const handleButtonClick = () => {
+    navigate(path); // Navigate to route
+  };
+
+  return (
+    <button
+      type="button"
+      onClick={handleButtonClick}
+      className={`
+        inline-flex items-center justify-center gap-2.5
+        px-2 py-1 md:px-4 md:py-2
+        rounded-2xl font-nunito font-normal text-white text-base
+        transition-all duration-300
+
+        ${
+          isActive
+            ? `backdrop-blur-[12px] backdrop-brightness-[108%] backdrop-saturate-[105%] backdrop-hue-rotate-[-3.6deg]
+               [-webkit-backdrop-filter:blur(12px)_brightness(108%)_saturate(105%)_hue-rotate(-3.6deg)]
+               shadow-[inset_0_1px_0_rgba(255,255,255,0.40),inset_1px_0_0_rgba(255,255,255,0.32),inset_0_-1px_8px_rgba(0,0,0,0.20),inset_-1px_0_8px_rgba(0,0,0,0.16)]
+               bg-[linear-gradient(90deg,rgba(151,0,255,0.2)_37%,rgba(0,183,159,0.2)_100%)]`
+            : ""
+        }
+
+        hover:backdrop-blur-[12px]
+        hover:backdrop-brightness-[108%]
+        hover:backdrop-saturate-[105%]
+        hover:backdrop-hue-rotate-[-3.6deg]
+        hover:shadow-[inset_0_1px_0_rgba(255,255,255,0.40),inset_1px_0_0_rgba(255,255,255,0.32),inset_0_-1px_8px_rgba(0,0,0,0.20),inset_-1px_0_8px_rgba(0,0,0,0.16)]
+        hover:bg-[linear-gradient(90deg,rgba(151,0,255,0.2)_37%,rgba(0,183,159,0.2)_100%)]
+      `}
+    >
+      {label}
+    </button>
+  );
+}
+
+export default RoutingMenuItem;
