2fe0550d0869e5024483822159cb8642957dfc21
AnaRuizS <aruiz6@uwo.ca>
2026-03-27 07:25:03 -0500

    Removida ruta portafolio, creada ruta contacto

--- adwa-web/src/Routes.jsx
+++ adwa-web/src/Routes.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Nosotros from "./pages/Nosotros";
-import Portafolio from "./pages/Portafolio";
 import DisenoGrafico from "./pages/DisenoGrafico";
 import Uxui from "./pages/Uxui";
+import Contacto from "./pages/Contacto";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/nosotros" element={<Nosotros />} />
-      <Route path="/portafolio" element={<Portafolio />} />
       <Route path="/diseno-grafico" element={<DisenoGrafico />}/>
       <Route path="/ux-ui" element={<Uxui />}/>
+      <Route path="/contacto" element={<Contacto/>}/>
     </Routes>
   );
 }
