da63b5f83ff45d6a1aef01f270f5c98c4ab7534d
AnaRuizS <aruiz6@uwo.ca>
2026-03-27 07:36:21 -0500

    Añadidas nuevas secciones al Home

--- adwa-web/src/pages/Home.jsx
+++ adwa-web/src/pages/Home.jsx
@@ -1,25 +1,28 @@
 import React from "react";
-import Navbar from "../components/Navbar";
-import PaidMediaSection from "../components/PaidMediaSection";
-import Hero from "../components/Hero";
+import Navbar2 from "../components/Navbar2";
+import PaidMediaSectionOP from "../components/PaidMediaSectionOP";
+import ConceptSection from "../components/ConceptSection";
 import Hero2 from "../components/Hero2";
 import SpotSection from "../components/SpotSection";
 import ProductoraSection from "../components/ProductoraSection";
 import IASection from "../components/IASection";
 import CampanasSection from '../components/CampanasSection';
-import Footer from "../components/Footer";
+import PartnersSection from "../components/PartnersSection";
+import Footer2 from "../components/Footer2";
 
 export default function Home() {
   return (
     <>
-        <Navbar/>
+        <Navbar2/>
         <Hero2/>
+        <ConceptSection/>
         <SpotSection/>
-        <PaidMediaSection/>
-        <ProductoraSection/>
-        <CampanasSection/>
         <IASection/>
-        <Footer/>
+        <PartnersSection/>
+        <PaidMediaSectionOP/>
+        <CampanasSection/>
+        <ProductoraSection/>
+        <Footer2/>
     </>
 );
 }
