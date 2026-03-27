ed1dfcaf7a3d6d7d9c260ce17b3f8b552333d14e
AnaRuizS <aruiz6@uwo.ca>
2026-03-27 07:33:37 -0500

    Cambiado estilo de HeroHeader para secciones diseño y UX UI

--- adwa-web/src/components/ui/HeroHeader.jsx
+++ adwa-web/src/components/ui/HeroHeader.jsx
@@ -4,18 +4,19 @@ const HeroHeader = ({
   title, 
   highlightedText, 
   backgroundImage,
+  containerHeight,
   indent = "md:pl-20 lg:pl-40"
 }) => {
   return (
     <div
-      className="relative flex items-center justify-center min-h-[60vh] section-wrapper-auto bg-cover bg-center mt-[-90px]"
+      className={`relative flex items-center justify-center ${containerHeight} section-wrapper-auto bg-cover bg-center mt-[-90px]`}
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
-      <div className="flex flex-col items-center text-center text-white max-w-4xl px-6 gap-10">
-        <h1 className="text-white text-[50px] leading-[50px] lg:text-[80px] lg:leading-[80px] tracking-[0.26px]">
+      <div className="flex flex-col items-center text-center text-white max-w-4xl px-6">
+        <h1 className="text-white text-[50px] leading-[40px] lg:text-[80px] lg:leading-[60px] tracking-[0.26px]">
           {title} <br />
-          <span className={`relative inline-block ${indent}`}>
-            <span className="tracking-[0.26px] bg-gradient-to-r from-[#9700FF] to-[#00B79F] bg-clip-text text-transparent">
+          <span className={`relative inline-block leading-[0px] ${indent}`}>
+            <span className="tracking-[0.26px] leading-[0px] bg-gradient-to-r from-[#9700FF] to-[#00B79F] bg-clip-text text-transparent">
               {highlightedText}
             </span>
           </span>
